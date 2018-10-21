c2a3689469f58a7890087b35f51a8ac8211779d7
romanova-m <9r.maria.n8@gmail.com>
2018-10-21 14:25:11 +0300

    small fix

--- src/main/java/mirea/pets/service/CartService.java
+++ src/main/java/mirea/pets/service/CartService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import javax.annotation.PostConstruct;
-import java.util.Collection;
 import java.util.Optional;
 
 @Controller
